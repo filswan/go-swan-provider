dc44f6bfc84fae0fb40dd0620d9032f9643404eb
DoraNebula <fchen@nbai.io>
2021-09-09 19:24:37 -0600

    separate test code from the others

---
+++ main/test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"swan-miner/common/utils"
+	"swan-miner/config"
+	"swan-miner/models"
+	"swan-miner/offlineDealAdmin"
+)
+
+func testRestApiAccessor() {
+	response := utils.HttpGet("https://jsonplaceholder.typicode.com/todos/1", "", "")
+	fmt.Println(response)
+	todo := models.Todo{1, 2, "lorem ipsum dolor sit amet", true}
+	response = utils.HttpPostNoToken("https://jsonplaceholder.typicode.com/todos", todo)
+	fmt.Println(response)
+
+	response = utils.HttpPut("https://jsonplaceholder.typicode.com/todos/1", "",todo)
+	fmt.Println(response)
+
+	title := utils.GetFieldFromJson(response,"title")
+	fmt.Println("title",title)
+
+	response = utils.HttpDelete("https://jsonplaceholder.typicode.com/todos/1", "",todo)
+	fmt.Println(response)
+}
+
+func testSwanClient() {
+	swanClient := utils.GetSwanClient()
+
+	//fmt.Println(swanClient)
+
+	mainConf := config.GetConfig().Main
+	swanClient.GetOfflineDeals(mainConf.MinerFid,"Waiting", "10")
+	swanClient.UpdateOfflineDealStatus("Completed","test note","2455")
+}
+
+func testAriaClient() {
+	swanClient := utils.GetSwanClient()
+
+	aria2Client := utils.GetAria2Client()
+	offlineDeal := &models.OfflineDeal{
+		Id: "163",
+		UserId: string(163),
+		SourceFileUrl: "https://file-examples-com.github.io/uploads/2020/03/file_example_WEBP_500kB.webp",
+	}
+
+	aria2Service := offlineDealAdmin.GetAria2Service()
+	aria2Service.StartDownloadForDeal(*offlineDeal, aria2Client, swanClient)
+	aria2Client.GetDownloadStatus("f80d913a4dff40651")
+}
+
+func testLotusClient() {
+
+}
