10f9214718bc71b30721667528a971ddd92cf8ff
DoraNebula <fchen@nbai.io>
2021-10-07 09:13:46 -0600

    fixed bugs

--- models/miner.go
+++ models/miner.go
@@ -6,5 +6,5 @@ type Miner struct {
 	BidMode             int    `json:"bid_mode"`
 	ExpectedSealingTime int    `json:"expected_sealing_time"`
 	StartEpoch          int    `json:"start_epoch"`
-	AutoBidDealPerDay   int    `toml:"auto_bid_deal_per_day"`
+	AutoBidTaskPerDay   int    `toml:"auto_bid_task_per_day"`
 }
