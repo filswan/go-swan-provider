57e869316b0bd35f1e26ea538134cf3f55635823
DoraNebula <fchen@nbai.io>
2021-10-08 13:53:41 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ wget https://github.com/filswan/go-swan-provider/releases/download/release-0.1.0
 wget https://github.com/filswan/go-swan-provider/releases/download/release-0.1.0-beta-rc1/aria2.conf
 wget https://github.com/filswan/go-swan-provider/releases/download/release-0.1.0-beta-rc1/aria2c.service
 
-sed -i 's/%%USER%%/'${USER}'/g' aria2c.service   # Set User & Group to value of $USER
+sed -i 's/%%USER%%/'${USER}'/g' ./aria2c.service   # Set User & Group to value of $USER
 
 if [ ! -d "/etc/aria2" ]; then
     sudo mkdir /etc/aria2
@@ -16,7 +16,7 @@ fi
 sudo chown $USER:$USER /etc/aria2/             # Change user authority to current user
 sudo touch /etc/aria2/aria2.session            # Create a session file
 sudo cp ./aria2.conf /etc/aria2/               # Copy config file
-sudo cp aria2c.service /etc/systemd/system/    # Copy service file
+sudo cp ./aria2c.service /etc/systemd/system/    # Copy service file
 sudo systemctl enable aria2c.service           # Set to start Aria2 automatically
 sudo systemctl start aria2c.service            # Start Aria2
 
