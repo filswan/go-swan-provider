18230efd96c9e57bea40fb0df45282c14e4d78f5
DoraNebula <90062637+DoraNebula@users.noreply.github.com>
2021-11-15 19:10:26 -0600

    update cost

--- service/aria2.go
+++ service/aria2.go
@@ -77,7 +77,7 @@ func (aria2Service *Aria2Service) CheckDownloadStatus4Deal(aria2Client *client.A
 
 	switch result.Status {
 	case ARIA2_TASK_STATUS_ERROR:
-		UpdateDealInfoAndLog(deal, DEAL_STATUS_DOWNLOAD_FAILED, &filePath, nil, result.Status, gid, result.ErrorCode, result.ErrorMessage)
+		UpdateDealInfoAndLog(deal, DEAL_STATUS_DOWNLOAD_FAILED, &filePath, result.Status, gid, result.ErrorCode, result.ErrorMessage)
 	case ARIA2_TASK_STATUS_ACTIVE:
 		fileSizeDownloaded := utils.GetFileSize(filePath)
 		completedLen := utils.GetInt64FromStr(file.CompletedLength)
@@ -89,17 +89,17 @@ func (aria2Service *Aria2Service) CheckDownloadStatus4Deal(aria2Client *client.A
 		fileSizeDownloaded = fileSizeDownloaded / 1024
 		note := fmt.Sprintf("downloading, complete: %.2f%%, speed: %dKiB, downloaded:%dKiB,%s,%s", completePercent, downloadSpeed, fileSizeDownloaded, result.Status, gid)
 		logs.GetLogger().Info(GetLog(deal, note))
-		UpdateDealInfoAndLog(deal, DEAL_STATUS_DOWNLOADING, &filePath, nil, gid)
+		UpdateDealInfoAndLog(deal, DEAL_STATUS_DOWNLOADING, &filePath, gid)
 	case ARIA2_TASK_STATUS_COMPLETE:
 		fileSizeDownloaded := utils.GetFileSize(filePath)
 		logs.GetLogger().Info(GetLog(deal, "downloaded"))
 		if fileSizeDownloaded >= 0 {
-			UpdateDealInfoAndLog(deal, DEAL_STATUS_DOWNLOADED, &filePath, nil, gid)
+			UpdateDealInfoAndLog(deal, DEAL_STATUS_DOWNLOADED, &filePath, gid)
 		} else {
-			UpdateDealInfoAndLog(deal, DEAL_STATUS_DOWNLOAD_FAILED, &filePath, nil, "file not found on its download path")
+			UpdateDealInfoAndLog(deal, DEAL_STATUS_DOWNLOAD_FAILED, &filePath, "file not found on its download path")
 		}
 	default:
-		UpdateDealInfoAndLog(deal, DEAL_STATUS_DOWNLOAD_FAILED, &filePath, nil, result.Status, gid, result.ErrorCode, result.ErrorMessage)
+		UpdateDealInfoAndLog(deal, DEAL_STATUS_DOWNLOAD_FAILED, &filePath, result.Status, gid, result.ErrorCode, result.ErrorMessage)
 	}
 }
 
