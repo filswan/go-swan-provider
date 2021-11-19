7c75cfda87264cf1f74522fe9894fde46f4ec6ad
DoraNebula <90062637+DoraNebula@users.noreply.github.com>
2021-11-19 09:41:33 -0600

    Update main.go

--- main.go
+++ main.go
@@ -20,13 +20,14 @@ func main() {
 	//LoadEnv()
 	//test.Test()
 	//config.InitConfig()
+	os.Setenv("GIN_MODE", "release")
 	service.AdminOfflineDeal()
 	createHttpServer()
 }
 
 func createHttpServer() {
 	r := gin.Default()
-	gin.SetMode(gin.ReleaseMode)
+	//gin.SetMode(gin.ReleaseMode)
 	r.Use(cors.Middleware(cors.Config{
 		Origins:         "*",
 		Methods:         "GET, PUT, POST, DELETE",
