450896ed0964f348725da5974e4055565194c920
DoraNebula <90062637+DoraNebula@users.noreply.github.com>
2022-05-05 14:01:32 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BINARY_NAME=swan-provider-0.2.1-linux-amd64
-TAG_NAME=v0.2.1-rc2
+TAG_NAME=v0.2.1
 
 wget https://github.com/filswan/go-swan-provider/releases/download/${TAG_NAME}/${BINARY_NAME}
 wget https://github.com/filswan/go-swan-provider/releases/download/${TAG_NAME}/aria2.conf
